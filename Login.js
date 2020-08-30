54b5c5ab694bce0889077b28cc18abb4d5406115
shwin320 <34398309+shwin320@users.noreply.github.com>
2020-08-29 20:39:48 -0400

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,32 @@
+var login = function(e){
+	e.preventDefault();
+	var user = $("#user").val();
+	var pass = $("#pass").val();
+	if(user == "athomas1"){
+		if(pass == "thomaslocker"){
+			localStorage.setItem("user", "shwin17MAGD");
+			window.location.href="home.html";
+		} else{
+			alert("You have entered an incorrect username/password! Please retry!");
+		};
+	} else if(user == "example1"){
+		if(pass == "demo"){
+			localStorage.setItem("user", "demo");
+			window.location.href="home.html";
+		} else{
+			alert("You have entered an incorrect username/password! Please retry!");
+		};
+	} else if(user == "lmtsd"){
+		if(pass == "public"){
+			localStorage.setItem("user", "lmtsd");
+			window.location.href="home.html";
+		} else{
+			alert("You have entered an incorrect username/password! Please retry!");
+		};
+	} else{
+		alert("You have entered an incorrect username/password! Please retry!");
+	};
+	if(localStorage.getItem("user") == "shwin17MAGD" || localStorage.getItem("user") == "demo" || localStorage.getItem("user") == "lmtsd"){
+		window.location.replace("page1.html");
+	}
+};
